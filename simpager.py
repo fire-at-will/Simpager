98ccde2570030d44867d4100529dd9847d661992
James <jgstew101@gmail.com>
2016-04-06 20:32:53 -0500

    error on user input line 60

--- simpager.py
+++ simpager.py
@@ -15,6 +15,11 @@ from rand import *
 from mfu import *
 from mru import *
 
+# global variables
+algorithms = []
+frames = 0
+page_array = []
+
 
 ##################################################
 #
@@ -22,19 +27,43 @@ from mru import *
 #
 ##################################################
 def main():
-    print "hello"
+    algorithms = []
 
     if len(sys.argv) == 2:
+        count = 0
+
+        #read file specs into variables
         for line in sys.stdin:
             #store variables
+            if count == 0:
+                page_array = line.split(' ')
+            elif count == 1:
+                frames = int(line)
+            else:
+                algorithms[count-2] = line
+            count = count + 1
     else:
-        print "READ IN"
+        count = 0
         while(True):
             line = raw_input()
+
+            #check for last input
             if line == "":
                 break
-            print line
 
+            #store variables
+            if count == 0:
+                page_array = line.split(' ')
+            elif count == 1:
+                frames = int(line)
+            else:
+                algorithms[count-2] = line #IndexError: list assignment index out of range
+            count = count + 1
+
+
+    print page_array
+    print frames
+    print algorithms
 
 # Execute main function
 if __name__ == "__main__": main()
