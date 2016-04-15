cbdc6c267a76b9416fd4e8fae9232643d482efe7
James <jgstew101@gmail.com>
2016-04-15 12:55:24 -0500

    need to test OPT now

--- opt.py
+++ opt.py
@@ -1,4 +1,5 @@
 def opt(page_array, frames):
+
     #map string array to ints
     page_array = map(int, page_array)
 
@@ -24,6 +25,22 @@ def opt(page_array, frames):
             page_faults = page_faults + 1
         cur_index = cur_index + 1
 
+    print "OPT: " + str(page_faults)
+
 def findIndex(cur_index, page_array, frame_array):
     cur_frame_index = 0
+    longest_till_occurence = frame_array[0] #initialize page with longest time to occ.
+    index_to_return = 0
+    max_index_count = 0
     for page_in_frame in frame_array:
+        index_count = 1
+        for page in page_array[cur_index+1:]: #hopefully start at cur_index?
+            if page == page_in_frame:
+                continue #break out of inner for loop
+            else:
+                index_count = index_count + 1
+        if index_count > max_index_count:
+            longest_till_occurence = page_in_frame
+            index_to_return = frame_array.index(longest_till_occurence)
+
+    return index_to_return
