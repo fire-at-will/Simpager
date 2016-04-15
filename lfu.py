dc199926e604029c7ac758b4c758f62d0266cde2
Will Taylor <will.taylor@tcu.edu>
2016-04-15 14:26:09 -0500

    Finished MFU and LFU

--- lfu.py
+++ lfu.py
@@ -1,2 +1,49 @@
-def lfu():
-    print "Least frequently used page replacement"
+def lfu(page_array, frames):
+    frame_array = []
+    frame_index = 0
+    page_faults = 0
+    insertions = 0
+
+    # Dictionary to hold page usage counts
+    pageUseCount = {}
+
+    for index, page in enumerate(page_array):
+
+        if page in frame_array:
+            pageUseCount[page] += 1     # Increment this page used
+            continue # should skip to next iteration
+        else:
+            if page in pageUseCount:
+                pageUseCount[page] += 1     # Increment this page used
+            else:
+                pageUseCount[page] = 1      # We have used this page once now
+
+            # Increment the number of page faults
+            page_faults = page_faults + 1
+
+            # Put new page into list of frames
+            if len(frame_array) < frames:
+                frame_array.append(page)
+            else:
+                # Determine victim page
+                victimIndex = determineLFUPageToKick(frame_array, pageUseCount)
+                frame_array[victimIndex] = page
+
+    print "LFU: " + str(page_faults)
+
+
+def determineLFUPageToKick(frame_array, pageUseCount):
+    accessCounts = []
+    for page in frame_array:
+        timesAccessed = pageUseCount[page]
+        accessCounts.append(timesAccessed)
+
+    minAccess = 10000
+    minIndex = 10000
+
+    for index, accessCount in enumerate(accessCounts):
+        if accessCount < minAccess:
+            minAccess = accessCount
+            minIndex = index
+
+    return minIndex
--- mfu.py
+++ mfu.py
@@ -1,2 +1,49 @@
-def mfu():
-    print "Most frequently used"
+def mfu(page_array, frames):
+    frame_array = []
+    frame_index = 0
+    page_faults = 0
+    insertions = 0
+
+    # Dictionary to hold page usage counts
+    pageUseCount = {}
+
+    for index, page in enumerate(page_array):
+
+        if page in frame_array:
+            pageUseCount[page] += 1     # Increment this page used
+            continue # should skip to next iteration
+        else:
+            if page in pageUseCount:
+                pageUseCount[page] += 1     # Increment this page used
+            else:
+                pageUseCount[page] = 1      # We have used this page once now
+
+            # Increment the number of page faults
+            page_faults = page_faults + 1
+
+            # Put new page into list of frames
+            if index <= frames:
+                frame_array.append(page)
+            else:
+                # Determine victim page
+                victimIndex = determineMFUPageToKick(frame_array, pageUseCount)
+                frame_array[victimIndex] = page
+
+    print "MFU: " + str(page_faults)
+
+
+def determineMFUPageToKick(frame_array, pageUseCount):
+    accessCounts = []
+    for page in frame_array:
+        timesAccessed = pageUseCount[page]
+        accessCounts.append(timesAccessed)
+
+    maxAccess = -1
+    maxIndex = -1
+
+    for index, accessCount in enumerate(accessCounts):
+        if accessCount > maxAccess:
+            maxAccess = accessCount
+            maxIndex = index
+
+    return maxIndex
