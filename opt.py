a6802b25bbded9bfef2bdcc1f85e95495991ce80
James <jgstew101@gmail.com>
2016-04-16 12:06:54 -0500

    OPT is so annoying

--- opt.py
+++ opt.py
@@ -7,12 +7,12 @@ def opt(page_array, frames):
     frame_index = 0
     page_faults = 0
     insertions = 0
-    cur_index = 0
+    cur_index = -1
 
     for page in page_array:
+        cur_index = cur_index + 1
         if page in frame_array:
             print frame_array
-            cur_index = cur_index + 1
             continue
         else:
             #if we know frame_array has no empty slots
@@ -26,24 +26,21 @@ def opt(page_array, frames):
             insertions = insertions + 1
             print frame_array
             page_faults = page_faults + 1
-        cur_index = cur_index + 1
 
     print "OPT: " + str(page_faults)
 
 def findIndex(cur_index, page_array, frame_array):
-    longest_till_occurence = frame_array[0] #initialize page
     index_to_return = 0
     max_index_count = 0
     for page_in_frame in frame_array:
         index_count = 0
-        for page in page_array[cur_index+1:]: #hopefully start at cur_index?
+        for page in page_array[cur_index:]:
             if page == page_in_frame:
-                continue #break out of inner for loop
+                continue
             else:
                 index_count = index_count + 1
         if index_count > max_index_count:
             max_index_count = index_count
-            longest_till_occurence = page_in_frame
-            index_to_return = frame_array.index(longest_till_occurence)
+            index_to_return = frame_array.index(page_in_frame)
 
     return index_to_return
